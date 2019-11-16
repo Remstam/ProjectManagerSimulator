527bdc8cd667d0744288aac67154fd119b799fd2
vangogih <vangogih@yandex.ru>
2019-11-16 23:47:01 +0300

    Adding ImportedAssets to VCS: - Odin - DoTween - Hierarchy pro

--- Assets/Code/Test.cs
+++ Assets/Code/Test.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+
+    [Button]
+    private void Hello()
+    {
+        Debug.Log("Hello World");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
